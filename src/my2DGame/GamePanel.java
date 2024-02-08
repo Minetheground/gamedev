758ea909e4a2d1f18e92d8196fac9549e33be49c
Conor <Conor@DESKTOP-S2TIGS9>
2024-02-08 23:34:13 +0000

    test

---
+++ src/my2DGame/GamePanel.java
@@ -0,0 +1,17 @@
+package my2DGame;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	
+	final int defaultTileSize = 16;
+	final int scale = 3;
+	
+	final int tileSize = defaultTileSize * scale;
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = maxScreenCol * tileSize;
+	final int screenHeight = maxScreenRow * tileSize;
+	
+
+}
---
+++ src/my2DGame/Main.java
@@ -0,0 +1,20 @@
+package my2DGame;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setTitle("new Gae");
+		
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+	}
+
+}
