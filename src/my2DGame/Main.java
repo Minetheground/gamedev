870430059979bf65e87ed7a7c877020072e0a148
Conor <Conor@DESKTOP-S2TIGS9>
2024-02-08 23:37:31 +0000

    test

--- src/my2DGame/Main.java
+++ src/my2DGame/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		frame.setTitle("new Gae");
+		frame.setTitle("new Game");
 		
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
