129be5bec824804b008626240df1e2f8807fa7e4
Minethegroud <conorkeanestack@gmail.com>
2024-03-22 21:32:21 +0000

    added comments to understand some concepts

--- src/my2DGame/Main.java
+++ src/my2DGame/Main.java
@@ -2,15 +2,20 @@ package my2DGame;
 
 import javax.swing.JFrame;
 
+
+
 public class Main {
 	
-	
+	//Beginning of main class
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// Beginning of main method
+		
+		JFrame frame = new JFrame();  // We plugin a class called JFrame to assing a variable called frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // we begin to add some properies 
 		frame.setResizable(false);
-		frame.setTitle("New Game");
+		frame.setTitle("New Game");   
 		
 		GamePanel GamePanel = new GamePanel();
 		frame.add(GamePanel);
