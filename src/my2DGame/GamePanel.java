b6a7fd68f674c9db71a27be79cdca5479f506857
Minetheground <conorkeanestack@gmail.com>
2024-03-23 00:18:17 +0000

    more comments

--- src/my2DGame/GamePanel.java
+++ src/my2DGame/GamePanel.java
@@ -5,17 +5,17 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel{  // we extend from the class JPanel so that we can add Content.
+										//so then we use a bit of math 
+	final int defaultTileSize = 16;		//so then we add a tile of size 16.	
+	final int scale = 3;                //to which is scaled 3 times.
 	
-	final int defaultTileSize = 16;
-	final int scale = 3;
-	
-	final int tileSize = defaultTileSize * scale;
+	final int tileSize = defaultTileSize * scale;  // then we multiply defaultTileSize and scale, to add in total 48 tiles to the panel
 	final int maxScreenCol = 16;
 	final int maxScreenRow = 12;
 	final int screenWidth = maxScreenCol * tileSize;
 	final int screenHeight = maxScreenRow * tileSize;
-	
+																	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
