f7ace58e66f9ca03a1973930a16654220af28f6c
Minethegroud <63200516+Minethegroud@users.noreply.github.com>
2024-04-05 17:04:06 +0100

    here is the Minecraft Code from 2009 to experiment with

---
+++ rubydung/com/mojang/minecraft/MinecraftApplet.java
@@ -0,0 +1,11 @@
+package com.mojang.minecraft;
+
+import java.applet.Applet;
+
+public class MinecraftApplet extends Applet {}
+
+
+/* Location:              /home/minetheground/Downloads/rd-161348-launcher.jar!/com/mojang/minecraft/MinecraftApplet.class
+ * Java compiler version: 6 (50.0)
+ * JD-Core Version:       1.1.3
+ */
